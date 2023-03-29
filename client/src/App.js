13c0c800e0a49a16e02b7b9e25457780c9b6f0c0
Ananth Chandra <ananthinindia@gmail.com>
2023-03-29 23:02:00 +0530

    improving Toss Page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OddEvenGame from './OddEvenGame';
+
+
 import './App.css';
 
 function App() {
---
+++ client/src/components/OddEvenGame.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+
+
+
+
+
+
+function OddEvenGame() {
+    const [playerChoice, setPlayerChoice] = useState('odd');
+    const [playerNumber, setPlayerNumber] = useState(0);
+    const [computerNumber, setComputerNumber] = useState(0);
+    const [result, setResult] = useState('');
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      const computerNumber = Math.floor(Math.random() * 7);
+      setComputerNumber(computerNumber);
+      const sum = playerNumber + computerNumber;
+      const isEven = sum % 2 === 0;
+      const playerWins = (playerChoice === 'even' && isEven) || (playerChoice === 'odd' && !isEven);
+      setResult(playerWins ? 'You win!' : 'You lose!');
+    }
+  
+    return (
+      <div>
+        <h1>Odd or Even Game</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Choose odd or even:
+            <select value={playerChoice} onChange={(event) => setPlayerChoice(event.target.value)}>
+              <option value="odd">Odd</option>
+              <option value="even">Even</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Your number (0-6):
+            <input type="number" min="0" max="6" value={playerNumber} onChange={(event) => setPlayerNumber(parseInt(event.target.value, 10))} />
+          </label>
+          <br />
+          <label>
+            Computer's number (0-6):
+            <input type="number" min="0" max="6" readOnly value={computerNumber} />
+          </label>
+          <br />
+          <button type="submit">Play</button>
+        </form>
+        <div>{result}</div>
+      </div>
+    );
+  }
+
+
+  export default OddEvenGame;
+
+  
